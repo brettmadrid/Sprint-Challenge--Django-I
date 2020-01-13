f39ac22f18941a9596ce05cbef7773ef224114e6
brettmadrid <brettmadrid9@gmail.com>
2020-01-12 21:58:38 -0800

    installed dj_database_url with 'pipenv install dj_database_url' command

--- djorg/settings.py
+++ djorg/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3=%6=z73ag=ra8@si5p^nerrm7lj#ck1txsi%*=59c^v^d9%pd'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
